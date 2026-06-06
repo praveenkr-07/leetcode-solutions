6dec280103622902f8d5355bfc92a861ab112c33
praveenkr-07 <praveenmahour098@outlook.com>
2026-06-07 00:37:23 +0530

    Create 02_insert_in_BST.cpp

---
+++ Binary Search Trees/02_insert_in_BST.cpp
@@ -0,0 +1,20 @@
+
+class Solution {
+public:
+    TreeNode* insertIntoBST(TreeNode* root, int val) {
+        if(root==NULL) root = new TreeNode(val);
+
+        else {
+            if(val<root->val){
+                if(root->left==NULL) root->left = new TreeNode(val);
+                else insertIntoBST(root->left,val);
+            }
+            else{
+                if(root->right==NULL) root->right = new TreeNode(val);
+                else insertIntoBST(root->right,val);
+            }
+        }
+        return root;
+        
+    }
+};
