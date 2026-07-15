122a31987837692b2b0d57084f0157a851828631
praveenkr-07 <praveenmahour098@outlook.com>
2026-07-15 22:56:05 +0530

    Add minimum insertions and deletions DP
    Add two dynamic programming solutions:
    - Minimum Insertion Steps to Make a String Palindrome
    - Minimum Deletions to Make Strings Same
    
    Both solutions use Longest Common Subsequence (LCS) approach to solve the respective string manipulation problems.

---
+++ DynamicProgramming/32_Minimum Insertion Steps to Make a String Palindrome.cpp
@@ -0,0 +1,39 @@
+class Solution
+{
+  int longestCommonSubsequence(string text1, string text2)
+  {
+    int idx1 = text1.length();
+    int idx2 = text2.length();
+    vector<vector<int>> dp;
+    dp.clear();
+    dp.resize(idx1 + 1, vector<int>(idx2 + 1, 0));
+    for (int i1 = 0; i1 < idx1; i1++)
+      dp[i1][0] = 0;
+    for (int i2 = 0; i2 < idx2; i2++)
+      dp[0][i2] = 0;
+    for (int i1 = 1; i1 <= idx1; i1++)
+    {
+      for (int i2 = 1; i2 <= idx2; i2++)
+      {
+        if (text1[i1 - 1] == text2[i2 - 1])
+          dp[i1][i2] = 1 + dp[i1 - 1][i2 - 1];
+        else
+          dp[i1][i2] = max(dp[i1 - 1][i2], dp[i1][i2 - 1]);
+      }
+    }
+    return dp[idx1][idx2];
+  }
+  int longestPalindromeSubseq(string s)
+  {
+    string s1 = s;
+    reverse(s1.begin(), s1.end());
+    return longestCommonSubsequence(s, s1);
+  }
+
+public:
+  int minInsertions(string s)
+  {
+    int longestPallindromeSubseq = longestPalindromeSubseq(s);
+    return s.length() - longestPallindromeSubseq;
+  }
+};
